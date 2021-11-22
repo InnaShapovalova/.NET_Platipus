9f1f14fe67f8e5cc266edeb554a33f139caba210
InnaShapovalova <74597261+InnaShapovalova@users.noreply.github.com>
2021-11-22 23:31:26 +0200

    Ready task

---
+++ WebService/WebService/Controllers/ReverseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReverseController : Controller
+    {
+        private readonly ILogger<ReverseController> _logger;
+
+        public ReverseController(ILogger<ReverseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get([FromQuery] string data)
+        {
+            if(int.TryParse(data, out int number))
+            {
+                return Json(new { result = Math.Sqrt(number) });
+            }
+
+            return Json(new { result = $"{new string(data.Reverse().ToArray())}" });
+        }
+    }
+}
